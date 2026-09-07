8d2590bf17f7578c0ab23606f49525fc51d8ec0a
Urvashi Chauhan <chauhanurvashi282@gmail.com>
2026-09-07 22:44:24 +0530

    Add files via upload

---
+++ Day29question1.c
@@ -0,0 +1,25 @@
+//Q57: Find the sum of array elements.
+#include <stdio.h>
+
+int main()
+{
+    int n, i, sum = 0;
+
+    scanf("%d", &n);
+
+    int a[n];
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        sum = sum + a[i];
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
