0ad4c0ca7096b16560e8facebb8efd7d2adb4d9c
Urvashi Chauhan <chauhanurvashi282@gmail.com>
2026-09-08 22:55:51 +0530

    Add files via upload

---
+++ Day30question1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    if (scanf("%d", &n) != 1) {
+        return 1;
+    }
+    
+    int arr[n];
+    int even_count = 0;
+    int odd_count = 0;
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        
+        if (arr[i] % 2 == 0) {
+            even_count++;
+        } else {
+            odd_count++;
+        }
+    }
+    printf("Even=%d, Odd=%d\n", even_count, odd_count);
+    
+    return 0;
+}
