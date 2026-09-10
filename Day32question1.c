2246ad1d009890db343e6f1c1cd097e1d2690aef
Urvashi Chauhan <chauhanurvashi282@gmail.com>
2026-09-10 13:17:10 +0530

    Add files via upload

---
+++ Day32question1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    if (scanf("%d", &a) != 1) return 0;
+    int c[a];
+    for (int i = 0; i < a; i++) {
+        scanf("%d", &c[i]);
+    }
+    if (scanf("%d", &b) != 1) return 0;
+    int d[b];
+    for (int i = 0; i < b; i++) {
+        scanf("%d", &d[i]);
+    }
+    for (int i = 0; i < a; i++) {
+        printf("%d ", c[i]);
+    }
+    for (int i = 0; i < b; i++) {
+        printf("%d%s", d[i], (i == b - 1) ? "" : " ");
+    }
+    printf("\n");
+    return 0;
+}
