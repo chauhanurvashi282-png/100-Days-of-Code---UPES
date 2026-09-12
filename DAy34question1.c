7f94d6ece52c48d53bf51f99928220f154283d6f
Urvashi Chauhan <chauhanurvashi282@gmail.com>
2026-09-12 22:00:01 +0530

    Add files via upload

---
+++ DAy34question1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n, a[1000], p, e, i;
+    
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    
+    scanf("%d %d", &p, &e);
+    
+    for (i = n; i > p; i--) {
+        a[i] = a[i - 1];
+    }
+    
+    a[p] = e;
+    n++;
+    
+    for (i = 0; i < n; i++) {
+        printf("%d", a[i]);
+        if (i < n - 1) {
+            printf(" ");
+        }
+    }
+    printf("\n");
+    
+    return 0;
+}
