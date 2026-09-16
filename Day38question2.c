f293ea0edc5a75ebaad4ab8e669aebcb53e8c340
Urvashi Chauhan <chauhanurvashi282@gmail.com>
2026-09-16 22:37:05 +0530

    Add files via upload

---
+++ Day38question2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    scanf("%d %d", &r, &c);
+    
+    int mat[100][100];
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &mat[i][j]);
+        }
+    }
+    
+    if (r != c) {
+        printf("False\n");
+        return 0;
+    }
+    
+    int symmetric = 1;
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            if (mat[i][j] != mat[j][i]) {
+                symmetric = 0;
+                break;
+            }
+        }
+    }
+    
+    if (symmetric) {
+        printf("True\n");
+    } else {
+        printf("False\n");
+    }
+    
+    return 0;
+}
