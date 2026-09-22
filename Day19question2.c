d72e917098e720b3b7c91014326068b11cb45382
Urvashi Chauhan <chauhanurvashi282@gmail.com>
2026-09-22 21:09:41 +0530

    Update Day19question2.c

--- Day19question2.c
+++ Day19question2.c
@@ -4,24 +4,24 @@
 int main() {
     int num, sum = 0, remainder;
 
-    // Read the input number from the user
+    
     if (scanf("%d", &num) != 1) {
         return 1; 
     }
 
-    // Handle negative numbers by converting to positive
+    
     if (num < 0) {
         num = -num;
     }
 
-    // Process each digit using a loop
+    
     while (num > 0) {
-        remainder = num % 10; // Extract the last digit
-        sum = sum + remainder; // Add digit to sum
-        num = num / 10;       // Remove the last digit
+        remainder = num % 10; 
+        sum = sum + remainder; 
+        num = num / 10;      
     }
 
-    // Print the final result
+    
     printf("%d\n", sum);
 
     return 0;
