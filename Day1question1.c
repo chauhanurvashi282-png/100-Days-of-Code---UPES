b3fa4a779d3126dcfb73921424cd88173cd076c5
Urvashi Chauhan <chauhanurvashi282@gmail.com>
2026-09-22 21:07:38 +0530

    Update Day1question1.c

--- Day1question1.c
+++ Day1question1.c
@@ -4,17 +4,17 @@
 int main() {
     float num1, num2, sum;
 
-    // Input two numbers from the user
+    
     printf("Enter first number: ");
     scanf("%f", &num1);
 
     printf("Enter second number: ");
     scanf("%f", &num2);
 
-    // Calculate the sum
+    
     sum = num1 + num2;
 
-    // Display the result
+    
     printf("The sum is: %.2f\n", sum);
 
     return 0;
