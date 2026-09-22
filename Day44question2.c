fa5bfdec48bd965b8320ced140451d67d31c2bf0
Urvashi Chauhan <chauhanurvashi282@gmail.com>
2026-09-22 22:35:42 +0530

    Add files via upload

---
+++ Day44question2.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main() {
+    char text[100];
+
+    fgets(text, 100, stdin);
+
+    int i = 0;
+    while (text[i] != '\0' && text[i] != '\n') {
+        if (text[i] == ' ') {
+            text[i] = '-';
+        }
+        printf("%c", text[i]);
+        i++;
+    }
+    printf("\n");
+
+    return 0;
+}
