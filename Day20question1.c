29a6ef82b8bafc4c5c4bb69cb427c30ed33198a8
Urvashi Chauhan <chauhanurvashi282@gmail.com>
2026-09-22 21:08:59 +0530

    Update Day20question1.c

--- Day20question1.c
+++ Day20question1.c
@@ -5,20 +5,20 @@ int main() {
     int num, digit;
     int product = 1;
 
-    // Take input from the user
+    
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    // Handle negative numbers
+    
     if (num < 0) {
         num = -num;
     }
 
-    // Process each digit
+    
     while (num > 0) {
         digit = num % 10; 
         
-        // Check if the digit is odd
+        
         if (digit % 2 != 0) {
             product *= digit;
         }
@@ -26,7 +26,7 @@ int main() {
         num /= 10; 
     }
 
-    // Print the final result
+    
     printf("Product of odd digits: %d\n", product);
 
     return 0;
