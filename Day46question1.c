9ca7577d1bffec1b527ee1529e7e77fa125ab74f
Urvashi Chauhan <chauhanurvashi282@gmail.com>
2026-09-24 19:34:13 +0530

    Add files via upload

---
+++ Day46question1.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+int main() 
+{
+    char str[100];
+    int i, j = 0;
+
+    if (scanf("%99s", str) != 1) 
+    {
+        return 0;
+    }
+
+    for (i = 0; str[i] != '\0'; i++) 
+    {
+        char ch = str[i];
+        if (ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u' &&
+            ch != 'A' && ch != 'E' && ch != 'I' && ch != 'O' && ch != 'U')
+             {
+            str[j++] = str[i];
+        }
+    }
+    str[j] = '\0';
+
+    printf("%s\n", str);
+
+    return 0;
+}
