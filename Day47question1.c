2b898f848fa4f94f3844dd3aea5618d1be83ae45
Urvashi Chauhan <chauhanurvashi282@gmail.com>
2026-09-26 21:46:50 +0530

    Add files via upload

---
+++ Day47question1.c
@@ -0,0 +1,41 @@
+
+#include <stdio.h>
+
+int main()
+ {
+    char str1[100], str2[100];
+    int count[256] = {0};
+    int i = 0, j = 0;
+
+    scanf("%s", str1);
+    scanf("%s", str2);
+
+    while (str1[i] != '\0')
+     {
+        count[(unsigned char)str1[i]]++;
+        i++;
+    }
+
+    while (str2[j] != '\0') 
+    {
+        count[(unsigned char)str2[j]]--;
+        j++;
+    }
+
+    if (i != j)
+     {
+        printf("Not anagrams\n");
+        return 0;
+    }
+
+    for (int k = 0; k < 256; k++)
+     {
+        if (count[k] != 0) {
+            printf("Not anagrams\n");
+            return 0;
+        }
+    }
+
+    printf("Anagrams\n");
+    return 0;
+}
